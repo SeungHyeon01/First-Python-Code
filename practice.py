3a846562816e0a2ca616492f8319d8554a9dd611
Woo seung Hyeon <dntmdgus2006@gmail.com>
2023-03-03 01:49:34 +0900

    first commit

--- practice.py
+++ practice.py
@@ -187,13 +187,3 @@ print(python.find("Java")) # 원하는 값이 없는 경우 -1을 반환
 
 print(python.count("n"))
 '''
-jumin = '940917-1234567'
-
-print("성별 : " +jumin[7])
-print("연 :"+ jumin[0:2] ) # 0~2 직적의 인덱스 까지 즉 0,1 인덱스 값을 가져옵니다.
-print("월 : "+ jumin[2:4])
-print("일 : "+jumin[4:6])
-
-print("생년월일 : "+jumin[:6]) # 처움부터 6 직전까지
-print("뒤 7자리 : "+jumin[7:]) # 7부터 끝까지 
-print(" 뒤 7자리 (뒤에부터)" + jumin[-7:]) # 맨 뒤에서 7번째 부터 끝까지
